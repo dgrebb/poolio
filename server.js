342b9374f4d61ccfceed61eca7980488e799a081
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-10-18 19:37:40 -0400

    adding routes and titles for application views

--- server.js
+++ server.js
@@ -10,5 +10,17 @@ app.listen(3000, () => {
 });
 
 app.get('/', ( req, res ) => {
-    res.render('home.twig', { title: 'hello world' });
+    res.render('home.twig', { title: 'Poolio!' });
+});
+
+app.get('/new-game', ( req, res ) => {
+    res.render('game--new.twig', { title: 'Start a new game!' });
+});
+
+app.get('/active-game', ( req, res ) => {
+    res.render('game--active.twig', { title: 'Let the games begin!' });
+});
+
+app.get('/leaderboard', ( req, res ) => {
+    res.render('leaderboard.twig', { title: 'Winners!' });
 });
