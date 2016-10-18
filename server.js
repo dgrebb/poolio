3e76258694681201e18d597405edceef3dbbef71
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-10-18 11:20:38 -0400

    initial express application

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+const express = require('express');
+const twig = require('twig');
+const bodyParser = require('body-parser');
+const db = require('redis').createClient();
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Poolio is ready at http://localhost:3000');
+});
