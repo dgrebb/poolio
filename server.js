d78c01dc2302067d35a80ed673a21ce72f8e8730
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-10-18 19:50:38 -0400

    adding database checks for application readiness and adjusting spacing in server.js

--- server.js
+++ server.js
@@ -5,22 +5,28 @@ const db = require('redis').createClient();
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Poolio is ready at http://localhost:3000');
+db.on('error', ( err ) => {
+	console.log('Poolio encountered an error connecting to the database: ' + err);
 });
 
+db.on('ready', () => {
+	app.listen(3000, () => {
+		console.log('Poolio is ready at http://localhost:3000');
+	});
+})
+
 app.get('/', ( req, res ) => {
-    res.render('home.twig', { title: 'Poolio!' });
+	res.render('home.twig', { title: 'Poolio!' });
 });
 
 app.get('/new-game', ( req, res ) => {
-    res.render('game--new.twig', { title: 'Start a new game!' });
+	res.render('game--new.twig', { title: 'Start a new game!' });
 });
 
 app.get('/active-game', ( req, res ) => {
-    res.render('game--active.twig', { title: 'Let the games begin!' });
+	res.render('game--active.twig', { title: 'Let the games begin!' });
 });
 
 app.get('/leaderboard', ( req, res ) => {
-    res.render('leaderboard.twig', { title: 'Winners!' });
+	res.render('leaderboard.twig', { title: 'Winners!' });
 });
