68b4eb077cc5beafc9d372a0cf2db40c105846d8
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-10-18 23:00:17 -0400

    limiting player selection for new game via jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
 $(function() {
-    console.log('ready');
+    $('.pi-form--game-start input:checkbox').on('click', function() {
+        var limit = $('input:checkbox:checked').length >= 2;
+        $(".pi-form--game-start input:checkbox").not(":checked").attr("disabled", limit);
+    });
 });
