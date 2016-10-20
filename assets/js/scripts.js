66f333fb6d1a88c7b9acc609e6a315e4e6c9db46
Dan Grebb <dan.grebb@thinkbrownstone.com>
2016-10-19 22:51:23 -0400

    fixing a missing bracket; sometimes the small things are easiest to overlook

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 $(function() {
+
     $('.pi-form--game-start input:checkbox').on('click', function() {
 
         var min = $('input:checkbox:checked').length !== 2;
-        $('.pi-form--game-start button[type="submit"').attr('disabled', min);
+        $('.pi-form--game-start button[type="submit"]').attr('disabled', min);
 
         var limit = $('input:checkbox:checked').length >= 2;
         $('.pi-form--game-start input:checkbox').not(':checked').attr('disabled', limit);
