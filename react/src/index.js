7edda7a3d5d8d37605a690a47943e3a9c558a909
Dan Grebb <dan.grebb@thinkbrownstone.com>
2017-01-25 16:18:32 -0500

    main react hello world application

--- react/src/index.js
+++ react/src/index.js
@@ -1,1 +1,31 @@
-console.log('hello world');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Main application container.
+ * @type {object}
+ */
+const container = document.querySelector('.container');
+
+/**
+ * Main Application class.
+ */
+class App extends Component {
+
+  render () {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+
+};
+
+/**
+ * Mount main application node to the DOM.
+ */
+ReactDOM.render(
+  <App />,
+  container
+);
